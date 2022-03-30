c21c47155f260aa6e54f764bc0ff38a3c08ac39b
Samantha Ortiz <samanthaortiz@bartonhollowmbp.lan>
2022-03-29 22:26:45 -0400

    Set cors header

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const {getTreasuryRates} = require('./handlers')
 const app = express()
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://client-rate-calculator.herokuapp.com/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
